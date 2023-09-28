bc6c965f2ff51965aa4eca1bce535d11eed58803
Ahmad Faour <ahmad.faour@polymtl.ca>
2023-09-28 15:55:38 -0400

    WIP
    Change-Id: I1312604fcb21a0d6e5765b0ad67e2a52e7474e72

--- analysis/org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/criticalpath/AbstractCriticalPathModule.java
+++ analysis/org.eclipse.tracecompass.analysis.graph.core/src/org/eclipse/tracecompass/analysis/graph/core/criticalpath/AbstractCriticalPathModule.java
@@ -175,7 +175,10 @@ public abstract class AbstractCriticalPathModule extends TmfAbstractAnalysisModu
         return true;
     }
 
-    private @Nullable ITmfGraph createGraph() {
+    /**
+     * @since 4.2
+     */
+    public @Nullable ITmfGraph createGraph() {
         Path htFile = getHistoryTreeFilePath();
         ITmfTrace trace = fGraphModule.getTrace();
         if (trace == null) {
